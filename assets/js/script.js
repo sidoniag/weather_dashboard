23422fce4f32a36f2de9b157e630994c5d5027a5
idonia <sidoniag@gmail.com>
2020-10-13 21:22:49 -0500

    update readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,35 @@
 // var userFormEl = document.querySelector("#user-form");
 // apiKey
 var apiKey = "1d4b1aacf5896d38bf0400bb7ba7aced";
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "+&appid=" + apiKey;
 
-// create input form 
-$("#searchBtn").on("click", function() {
-    var location = $("#location").val();
-    if(location === ''){
-        alert("Please enter a city");
-    }
-    $("#location").val("");
-    getForecast(location);
-});
 
-// create current locale and date container
-function getForecast(location) {
-    fetch(apiUrl)
-    .then(function(response) {
-        return response.json();
-    })
-}
-
-// create 5 day forecast container
-var displayWeather = function(forecast) {
-    if (forecast.length === 0) {
-        forecastContainerEl.textContent = "No data";
-        return;
-    }
-
-}
+// // create current locale and date container
+// function getForecast(location) {
+//     fetch(apiUrl)
+//     .then(function(response) {
+//         return response.json();
+//     })
+// }
 
 // create recent searches container
 
 // create api function
-var getCurrentWeather = function(location) {
+var getCurrentWeather = function(cityName, needToAddList) {
     // format the weather api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "+&appid=" + apiKey;
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
-        // request was successful
         if (response.ok) {
         response.json().then(function(data){
-            displayCurrentWeather(data,location);
+            var containerCurrentWeather = document.getElementById("containerCurrentWeather");
+            containerCurrentWeather.className = containerCurrentWeather.className.replace(/\binvisible\b/g, "visble");
+            document.getElementById("")
+
+        if (needToAddList && !selectedCitiesAr.includes(data.name)){
+            addCityToList(data.name);
+            addNewCityToLocalStorage(data.name);
+        }
         });
     }
         else {
@@ -55,7 +43,35 @@ var getCurrentWeather = function(location) {
     });
 }
 
-// store in localStorage 
+// create 5 day forecast container
+var get5DaysWeather = function(forecast) {
+    if (forecast.length === 0) {
+        forecastContainerEl.textContent = "No data";
+        return;
+    }
+},
 
-// event listeners
-// userFormEl.addEventListener("submit", formSubmitHandler);
+// get current weather
+var searchCity = function () {
+    if (!searchCityInputEl.value){ 
+    return;
+    }
+    var cityName = searchCityInputEl.value.trim();
+
+    getCurrentWeather(cityName, ture);
+    get5DaysWeather(cityName);
+
+    searchCityInputEl.value = "";
+}
+
+// create search city
+var selectCityFromList = function(event) {
+    var targetEl = event.target;
+    var cityName = event.target.getAttribute("data-city");
+    getCurrentWeather(cityName, false);
+    get5DaysWeather(cityName);
+}
+
+selectedCitiesEl.addEventListener("click", selectCityFromList);
+
+read CitiesList();
